c04653d925cad8ec9475034879909dfcd881e8f8
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-01-26 10:50:38 +0530

    na

---
+++ first.js
@@ -0,0 +1,234 @@
+/* let name = "Abhi Raghav"
+console.log(name);
+x=34;
+y=45;
+z=x+y;
+console.log(z);  
+{
+    let a =10;
+    console.log(a);
+}
+{
+    let a=67;
+    console.log(a);
+}
+
+const student = {
+
+     Name : "Abhishek Raghav",
+     class : "Btech Cse",
+     Roll : 2014306812,
+     Marks : 354, 
+     Cgpa : 8.6,
+     IsPass: true, 
+     Age : 23
+
+
+};
+student ["Age"] = student ["Age"] + 1;
+
+const Insta ={
+    Id: "Shradha Mam",
+    Isfollow: true,
+    Message : "yes",
+    Posts : 195,
+    followers : "569K",
+    following : 4,
+    Name : "Shradha Khapra",
+    Tag : "@shradhakhapra",
+    Designation:"Enterprenour",
+    Expreiance : "Apna College | Ex-Microsoft | DRDO",
+    Thought : "To educate someone is the highest privilege"
+
+
+    //this is variable in Javascript
+} */
+
+//Arithmetic operator in JAvaScript
+
+
+// let a=5;
+// let b=2;
+
+// console.log("a = ",a  ," & b = ",b);
+// console.log("a+b = " ,a+b );
+// console.log("a-b = ",a-b);
+// console.log("a*b = ",a*b);
+// console.log("a/b = ", a/b);
+// console.log("a%b = ",a%b);  //modulus operator
+// console.log("a ** b = ",a ** b);
+
+
+//Unary operator
+
+// let a=5;
+// let b=2;
+
+// console.log("a = ",a  ," & b = ",b);
+
+// a--;
+// console.log("decreament a = ",a);
+
+// b++;
+// console.log("increament b = ",b);
+
+
+
+// Assingment Operator
+
+// let a=5;
+// let b=2;
+
+// console.log("a = ",a  ," & b = ",b);
+
+//  a += 4;
+//  console.log(" a = ",a);
+
+// a -= 4;
+// console.log(" a = ",a);
+
+// a *= 4;
+// console.log(" a = ",a);
+
+// a %= 4;
+// console.log(" a = ",a);
+
+// a /= 4;
+// console.log(" a = ",a);
+
+// a **= 4;
+// console.log(" a = ",a);
+
+// Comparision operator
+// let a=5;
+// let b=6;
+
+// console.log(" a == b",a==b);
+// console.log("a!=b",a!=b);
+
+// logical operator
+
+// let a=5;
+// let b=6;
+
+// let cond1 = a < b;
+// let cond2 = a == 5;
+
+// console.log("cond1 && cond2 =  ", cond1 && cond2);
+
+// conditional ststement ;
+
+// let age =10
+// if (age>18){
+//       console.log("You are eligible for vote ");
+// }
+// else{
+//     console.log("You are not eligible for vote");
+// }
+
+
+// odd or even check
+
+// let a=13;
+// if (a%2===0){
+//     console.log(a,"is Even");
+// }
+// else{
+//     console.log(a," is Odd");
+// }
+
+// let mode ="light";
+// let  color
+
+// if(mode=== "black"){
+//     color ="white";
+// }
+// else if(mode=== "dark"){
+//     color = "Yellow";
+// }
+
+// else if(mode === "light"){
+//     color =" blue"
+// }
+
+// else{
+//     color = "green"
+// }
+// console.log(color);
+
+// if (mode==="light") console.log(mode);
+
+
+// ternary operator 
+
+// let age=13;
+
+// let result =age >=18 ? "Adult " : "Not adult";
+// console.log(result);
+
+//switch case
+
+// let expr= "Apple";
+
+// switch (expr) {
+//      case "Mango" :
+//         console.log("Mango price is 100 Rs per kg ");
+//      break;
+
+//      case "Apple" :
+//         console.log("Apple price is 1000 Rs per kg ");
+//      break ;
+
+//      case "Orange" :
+//         console.log("Orange price is 10 Rs per kg ");
+
+//         default:
+//             console.log("Sorry Not Available")
+
+//     }
+
+// Prompt and Alert 
+
+//alert("HELLO TO USER");
+
+// let result = prompt("Enter Your Name ");
+// console.log(result);
+
+// let num =prompt("Enter a Number");
+
+// if (num%5===0){
+//     console.log(num,"   is multiple of 5");
+// }
+// else{
+//     console.log(num," is not multiple of 5");
+// }
+
+// Practice Question
+
+// let score =prompt("Enter Your Percentage :  ");
+// let Grade;
+
+// if(score >= 90 && score <= 100){
+//     Grade ="A";
+// }
+// else if(score >= 70 && score <= 89){
+//     Grade ="B";
+// }
+// else if(score >= 60 && score <= 69){
+//     Grade ="C";
+// }
+// else if(score >= 33 && score <= 59){
+//     Grade ="D";
+// }
+// else {
+//     Grade = "Fail"
+// }
+// console.log("According to your Score :! Your Grade is = ", Grade);
+
+// for loop iin JavaScript
+
+for(let i=1; i<=5; i++)
+{
+    console.log("Hello JavaScript");
+}
+
